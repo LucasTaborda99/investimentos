6da6cac86eb93cb5596756aa45b53b741aed5998
Lucas Taborda da Rocha <llucas352@gmail.com>
2025-06-27 18:43:44 -0300

    fix: Edited EnvConfig.java

--- src/main/java/com/br/investimentos/config/EnvConfig.java
+++ src/main/java/com/br/investimentos/config/EnvConfig.java
@@ -4,11 +4,11 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class EnvConfig {
-
-    @Bean
-    public Dotenv dotenv() {
-        return Dotenv.load();
-    }
-}
+//@Configuration
+//public class EnvConfig {
+//
+//    @Bean
+//    public Dotenv dotenv() {
+//        return Dotenv.load();
+//    }
+//}
